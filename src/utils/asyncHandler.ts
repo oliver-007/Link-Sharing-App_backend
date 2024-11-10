e9d4ba93a995d8d2e4fd96eff76c4dfbf81c532a
oliver-007 <oliverahmed000@gmail.com>
2024-11-10 14:33:18 +0600

    define specific interface for error object

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -4,6 +4,10 @@ interface IAsyncHandlerFunc {
   (req: Request, res: Response, next: NextFunction): Promise<any>;
 }
 
+interface CustomError extends Error {
+  statusCode?: number;
+}
+
 // ++++++ PROMISE APPROACH +++++++
 // const asyncHandler = (func: IAsyncHandlerFunc): RequestHandler => {
 //   return (req, res, next) => {
@@ -18,10 +22,11 @@ const asyncHandler =
   async (req, res, next) => {
     try {
       await func(req, res, next);
-    } catch (error: any) {
-      res.status(error.statusCode || 401).json({
+    } catch (error: unknown) {
+      const customError = error as CustomError;
+      res.status(customError.statusCode || 500).json({
         success: false,
-        message: error.message,
+        message: customError.message || "An unexpected error occurred.",
       });
     }
   };
